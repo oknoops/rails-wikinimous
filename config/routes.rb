524f2b910b7add8017c73eb8db480f2cc89b0456
Oscar Knoops <oscarknoops@yahoo.com>
2018-10-31 12:14:59 +0100

    ex 2 xompleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # All my articles
+  get 'articles' => 'articles#index'
+
+  # create an article
+  get 'articles/new' => 'articles#new'
+  post 'articles' => 'articles#create'
+
+  # Show one article
+  get 'articles/:id' => 'articles#show', as: :article
+
+  # update article
+  get 'articles/:id/edit' => 'articles#edit'
+  patch 'articles/:id' => 'articles#update'
+
+  # destroy article
+  delete 'articles/:id' => 'articles#destroy', as: :delete_article
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  title = Faker::GameOfThrones.character
+  content = Faker::GameOfThrones.quote
+  article = Article.create(title: title, content: content)
+end
